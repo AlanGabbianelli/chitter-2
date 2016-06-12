0bbe6035100e8bf3c2c883f3404966dc2c9d5ebc
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-06-12 04:01:29 +0100

    Add year to chit/reply date

--- app/controllers/chits.rb
+++ app/controllers/chits.rb
@@ -3,7 +3,7 @@ require 'haml'
 class Chitter < Sinatra::Base
   post '/create_chit' do
     chit = Chit.create(chit_text: params[:chit_text],
-                       chit_time: Time.now.strftime("%d %b at %H:%M"),
+                       chit_time: Time.now.strftime("%d %b '%y at %H:%M"),
                        user_id: params[:user_id])
     chit.save
     redirect '/chits'
--- app/controllers/replies.rb
+++ app/controllers/replies.rb
@@ -1,7 +1,7 @@
 class Chitter < Sinatra::Base
   post '/create_reply' do
     reply = Reply.create(reply_text: params[:reply_text],
-                         reply_time: Time.now.strftime("%d %b at %H:%M"),
+                         reply_time: Time.now.strftime("%d %b '%y at %H:%M"),
                          chit_id: params[:chit_id],
                          user_id: params[:user_id])
     reply.save
